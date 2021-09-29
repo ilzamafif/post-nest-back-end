f1686fd7f60900a2e841c310f99d9bf0815f316e
ilzamafif <ilzamafif27@gmail.com>
2021-09-29 08:03:33 +0700

    memperbarui package bcrypt dan implementasi hash passsword

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -11,6 +12,7 @@ export class UserService {
     @InjectRepository(User) private userRepo: Repository<User>
   ) { }
   create(createUserDto: CreateUserDto) {
+    createUserDto.password = this.hash(createUserDto.password)
     return this.userRepo.save(createUserDto);
   }
 
@@ -24,11 +26,23 @@ export class UserService {
 
   update(id: number, updateUserDto: UpdateUserDto) {
     updateUserDto.id = id;
+    if (updateUserDto.password) {
+      updateUserDto.password = this.hash(updateUserDto.password)
+    }
     return this.userRepo.save(updateUserDto);
   }
 
   async remove(id: number) {
     let user = await this.userRepo.findOne(id);
     return this.userRepo.remove(user);
+  }
+
+  hash(plainPassword) {
+    const hash = bcrypt.hashSync(plainPassword, 10)
+    return hash
+  }
+  compare(plainPassword, hash) {
+    const valid = bcrypt.compareSync(plainPassword, hash)
+    return valid
   }
 }
