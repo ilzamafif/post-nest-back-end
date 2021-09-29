8e2f2b47595e3e71e5155e62818bbf6d3cf8147d
ilzamafif <ilzamafif27@gmail.com>
2021-09-29 08:27:53 +0700

    membuat validator unique field

---
+++ src/etc/validator/exist-validator.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { ValidationArguments, ValidatorConstraintInterface, ValidationOptions, registerDecorator, ValidatorConstraint } from 'class-validator';
+import { getConnection } from 'typeorm';
+
+@ValidatorConstraint({ async: true })
+@Injectable()
+export class ExistValidator implements ValidatorConstraintInterface {
+  async validate(value: any, args: ValidationArguments) {
+    let find = {
+      where: {
+        [args.constraints[1]]: args.value
+      }
+    }
+    let cek = await getConnection().getRepository(args.constraints[0]).findOne(find)
+    if (cek) return false
+    return true
+  }
+  defaultMessage(args: ValidationArguments) {
+    return args.property + ' ' + args.value + ' sudah digunakan'
+  }
+}
+
+export function IsExist(option: any, validationOption?: ValidationOptions) {
+  return function (object: any, propertyName: string) {
+    registerDecorator({
+      name: 'IsExist',
+      target: object.constructor,
+      propertyName: propertyName,
+      constraints: option,
+      options: validationOption,
+      validator: ExistValidator,
+      async: true
+    })
+  }
+}
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,4 +1,6 @@
 import { IsEmail, IsOptional, IsString, MaxLength, MinLength } from "class-validator"
+import { IsExist } from "src/etc/validator/exist-validator"
+import { User } from "../entities/user.entity"
 
 export class CreateUserDto {
   @IsOptional()
@@ -9,11 +11,13 @@ export class CreateUserDto {
   nama_user: string
 
   @IsEmail()
+  @IsExist([User, 'email'])
   email: string
 
   @IsString()
   @MinLength(6)
   @MaxLength(50)
+  @IsExist([User, 'username'])
   username: string
 
   @IsString()
