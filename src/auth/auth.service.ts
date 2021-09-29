ffc7c59e4ba13dfe55fee2e75743d9f59320ed12
ilzamafif <ilzamafif27@gmail.com>
2021-09-29 12:59:21 +0700

    integrasi jwt authentication

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService
+  ) {
+
+  }
+  async cekUser(username, password) {
+    let user = await this.userService.findUsername(username)
+    if (user) {
+      const valid = this.userService.compare(password, user.password)
+      if (valid) {
+        return user
+      }
+      else {
+        throw new BadRequestException({ message: 'Password salah' })
+      }
+    }
+    else {
+      throw new BadRequestException({ message: 'Username tidak ditemukan' })
+    }
+  }
+
+  generateToken(user: any) {
+    let dataToken = { id: user.id }
+    let token = this.jwtService.sign(dataToken)
+    return { token: token }
+  }
+}
