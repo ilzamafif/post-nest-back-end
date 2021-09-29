1e1881268bff224e8d1dc545a08272611950ca17
ilzamafif <ilzamafif27@gmail.com>
2021-09-29 07:34:16 +0700

    memastikan data benar valid

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,7 +1,23 @@
-export class CreateUserDto {
-  id: number
+import { IsEmail, IsOptional, IsString, MaxLength, MinLength } from "class-validator"
+
+export class UserDto {
+  @IsOptional()
+  id?: number
+
+  @IsString()
+  @MaxLength(200)
   nama_user: string
+
+  @IsEmail()
   email: string
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(50)
   username: string
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(32)
   password: string
 }
