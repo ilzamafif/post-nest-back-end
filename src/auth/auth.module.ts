60a796ef7bbae130f8290b80857a9ac10cdbc999
ilzamafif <ilzamafif27@gmail.com>
2021-09-29 12:17:25 +0700

    registrasikan stategi auth.module.ts

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
+  imports: [
+    UserModule,
+    ConfigModule.forRoot(),
+    JwtModule.register({
+      secret: process.env.JWT_KEY,
+      signOptions: {
+        expiresIn: '12h'
+      }
+    })
+  ],
+  exports: [AuthService, JwtModule]
 })
-export class AuthModule {}
+export class AuthModule { }
