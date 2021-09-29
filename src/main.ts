da0b0574c13ae38536a797d27bad53d69132ad46
ilzamafif <ilzamafif27@gmail.com>
2021-09-29 11:14:55 +0700

    integrasi dengan swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerCustomOptions, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,21 @@ async function bootstrap() {
       enableImplicitConversion: true
     }
   }))
+
+  const configSwagger = new DocumentBuilder()
+    .setTitle('Point of sale documentation')
+    .setDescription('Dokumentasi untuk api point of sale')
+    .setVersion('1.2')
+    .addBearerAuth()
+    .build()
+
+  const configCustomSwagger: SwaggerCustomOptions = {
+    swaggerOptions: { docExpansion: "none" }
+  }
+
+  const doc = SwaggerModule.createDocument(app, configSwagger)
+  SwaggerModule.setup('doc', app, doc, configCustomSwagger)
+
   await app.listen(3000);
 }
 bootstrap();
